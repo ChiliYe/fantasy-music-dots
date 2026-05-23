acbc7ff3aca87029ba79f9ee45e6f02c1fc42c80
ChiliYe <3388615157@qq.com>
2026-05-23 19:13:44 +0800

    dependence install

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 
 # 幻想音点 fantasy-music-dots
 
-由@ChiliYe开发的音乐节奏游戏
-a music game made by @ChiliYe
+made by @ChiliYe
---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+/** @format */
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
+	<StrictMode></StrictMode>,
+);
